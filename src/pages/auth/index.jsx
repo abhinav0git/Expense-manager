9ad9174c46622839fbee42b1d3c14d91608527cd
Abhinav <abhinav07c@gmail.com>
2024-03-24 14:09:22 +0530

    user friendly auth page

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -3,12 +3,14 @@ import { signInWithPopup } from "firebase/auth";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import "./styles.css";
+import { ReactComponent as GoogleIcon } from "../../assets/icons-google.svg";
 
 export const Auth = () => {
-    const navigate = useNavigate();
-    const { isAuth } = useGetUserInfo();
-  
-    const signInWithGoogle = async () => {
+  const navigate = useNavigate();
+  const { isAuth } = useGetUserInfo();
+
+  const signInWithGoogle = async () => {
+    try {
       const results = await signInWithPopup(auth, provider);
       const authInfo = {
         userID: results.user.uid,
@@ -18,19 +20,34 @@ export const Auth = () => {
       };
       localStorage.setItem("auth", JSON.stringify(authInfo));
       navigate("/expense-tracker");
-    };
-  
-    if (isAuth) {
-      return <Navigate to="/expense-tracker" />;
+    } catch (error) {
+      if (error.code === 'auth/cancelled-popup-request') {
+        console.log('Sign-in was cancelled.');
+      } else {
+        console.error('An error occurred during sign-in:', error);
+      }
     }
-  
-    return (
-      <div className="login-page">
-        <p>Sign In With Google to Continue</p>
-        <button className="login-with-google-btn" onClick={signInWithGoogle}>
-          {" "}
-          Sign In With Google
+  };
+
+  if (isAuth) {
+    return <Navigate to="/expense-tracker" />;
+  }
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2 className="login-title">Welcome</h2>
+        <p className="login-text">Sign In with Google to continue</p>
+        <button className="gsi-material-button" onClick={signInWithGoogle}>
+          <div className="gsi-material-button-state"></div>
+          <div className="gsi-material-button-content-wrapper">
+            <div className="gsi-material-button-icon">
+              <GoogleIcon className="thelogo"/>
+            </div>
+            <span className="gsi-material-button-contents">Sign In</span>
+          </div>
         </button>
       </div>
-    );
-  };
+    </div>
+  );
+};
