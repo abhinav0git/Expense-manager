a661c1499028736de92db8994331c1cf0df1ac07
abhinav <abhinav07c@gmail.com>
2025-01-15 22:11:37 +0530

    env changes

--- src/config/fire-config.js
+++ src/config/fire-config.js
@@ -13,13 +13,13 @@ import { getFirestore } from 'firebase/firestore'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClpGs-dcVdedPBuTWdnHjVVih-doL0m_o",
-  authDomain: "expense-teacker.firebaseapp.com",
-  projectId: "expense-teacker",
-  storageBucket: "expense-teacker.appspot.com",
-  messagingSenderId: "233979420003",
-  appId: "1:233979420003:web:427532e713cd93ffe98756",
-  measurementId: "G-X3ZFD3XZX6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
